1d519f9929d852d731916e7d3a6491511a4bd766
Romaine Allen <romallen1@gmail.com>
2026-06-23 14:08:32 +0900

    Enable in memory cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,8 +54,7 @@ Rails.application.configure do
   # want to log everything, set the level to "debug".
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
 
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :mem_cache_store, { size: 32.megabytes }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
